1d5b629afb1182ab6d0e156089b82bbc5f5cdcde
SahilMalekar <sahilmalekar07@gmail.com>
2021-07-27 05:42:20 -0700

    Added bcrypt hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =  require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -39,42 +40,48 @@ app.get("/register" , function(req , res){
 
 app.post("/login" , function(req , res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email : username} , function(err , foundUser){
         if(err)
         {
             console.log(err);
         }else{
-            if(foundUser.password === password)
-            {
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+               if(result === true)
+               {
                 res.render("secrets");
-            }else{
+               }else{
                 console.log("password not matched");
-            }
+               }
+            });
         }
     })
 })
 
 app.post("/register" , function(req , res){
-    const newUser = new User({
-        email : req.body.email,
-        password : md5(req.body.password),
-        full_name : req.body.full_name,
-        address : req.body.address,
-        city : req.body.city,
-        mobile_number : req.body.mnum,
-        zip : req.body.zip
-        });
-    
-        newUser.save(function(err){
-            if(err)
-            {
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        
+        const newUser = new User({
+            email : req.body.email,
+            password : hash,
+            full_name : req.body.full_name,
+            address : req.body.address,
+            city : req.body.city,
+            mobile_number : req.body.mnum,
+            zip : req.body.zip
+            });
+        
+            newUser.save(function(err){
+                if(err)
+                {
+                    console.log(err);
+                }else{
+                    res.render("secrets");
+                }
+            })
+    });
+  
 });
 
 
